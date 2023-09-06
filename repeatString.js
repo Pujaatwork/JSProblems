d2e3676ec64692a38b4ed68997167f3c247b55ee
Shivani Yadav <yadavshivani7695@gmail.com>
2023-09-06 17:21:11 +0530

    Add sum function

--- repeatString.js
+++ repeatString.js
@@ -1,11 +1,11 @@
 function repeat(str, num) {
     var output = "";
-    if (num > 0) {
+    if (num >=0) {
         for (var i = 0; i < num; i++){
             output = output + str;
 
     }
-    console.log("output");
+    console.log(output);
 }
 else {
     console.log("error");
@@ -15,5 +15,16 @@ else {
 repeat("hey", 4)
 repeat("hey", 0)
 repeat("hey", -1)
+
+//---------Sum Function--------------
+var sum =0;
+function i(nam){
+    for (var i = 1; i <=nam; i++){
+        sum =sum +i;
+    }
+console.log(sum);
+}
+i(4);
+
 
 

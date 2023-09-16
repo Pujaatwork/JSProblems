190f3cb1b8e59e18458ec06899e1e0f3a038369a
Shivani Yadav <yadavshivani7695@gmail.com>
2023-09-16 14:25:35 +0530

    ReverseString

--- repeatString.js
+++ repeatString.js
@@ -1,15 +1,15 @@
 function repeat(str, num) {
     var output = "";
-    if (num >=0) {
-        for (var i = 0; i < num; i++){
+    if (num >= 0) {
+        for (var i = 0; i < num; i++) {
             output = output + str;
 
+        }
+        console.log(output);
     }
-    console.log(output);
-}
-else {
-    console.log("error");
-}
+    else {
+        console.log("error");
+    }
 }
 
 repeat("hey", 4)
@@ -17,12 +17,12 @@ repeat("hey", 0)
 repeat("hey", -1)
 
 //---------Sum Function--------------
-var sum =0;
-function i(nam){
-    for (var i = 1; i <=nam; i++){
-        sum =sum +i;
+var sum = 0;
+function i(nam) {
+    for (var i = 1; i <= nam; i++) {
+        sum = sum + i;
     }
-console.log(sum);
+    console.log(sum);
 }
 i(4);
 
--- reverseString.js
+++ reverseString.js
@@ -1,5 +1,15 @@
-var reverseString = 'Shivani';
-for (var i = reverseString.length - 1; i >= 0; i--) {
-console.log(reverseString[i]);
+// var reverseString = 'Shivani';
+// for (var i = reverseString.length - 1; i >= 0; i--) {
+// console.log(reverseString[i]);
 
+// }
+function reverseString(str){
+    var string = "";
+    for (var i = str.length - 1; i >= 0; i--){
+        string += str[i];
+    }
+   
+console.log(string);
 }
+reverseString("Shivani Yadav");
+
